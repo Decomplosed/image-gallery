0e57fcc2629a1c69a712e3b7b7914a111c3b1583
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-17 14:28:31 +0200

    console log the data for now

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
     fetch(
       `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
     )
-  })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err))
+  }, [term])
 
   return (
     <div className='max-w-sm rounded overflow-hidden shadow-lg'>
