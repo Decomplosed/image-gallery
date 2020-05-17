3084775347090cd43abc76c601d2311918a48d17
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-17 14:15:59 +0200

    creating a template for card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <img
+        src='https://source.unsplash.com/random'
+        alt='unsplash'
+        className='w-full'
+      />
+      <div className='px-6 py-4'>
+        <div className='font-bold text-purple-500 text-xl mb-2'>
+          Photo by John Doe
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
