3b83bc2d0847c858cf754b1e25e19c6e02fb5dfa
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-17 14:36:07 +0200

    pass the src data stored from API

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,11 +3,7 @@ import React from 'react'
 const ImageCard = ({ image }) => {
   return (
     <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-      <img
-        src='https://source.unsplash.com/random'
-        alt='unsplash'
-        className='w-full'
-      />
+      <img src={image.webformatURL} alt='unsplash' className='w-full' />
       <div className='px-6 py-4'>
         <div className='font-bold text-purple-500 text-xl mb-2'>
           Photo by John Doe
