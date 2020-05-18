26c2331b744b8db775183a1c97d490a1dcc9fd55
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-18 20:41:27 +0200

    created input

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
 const ImageSearch = () => {
-  return <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
-    <form className="w-full max-w-sm">
-      
-    </form>
-  </div>
+  return (
+    <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+      <form className='w-full max-w-sm'>
+        <div className='flex items-center border-b border-b-2 border-teal-500 py-2'>
+          <input
+            type='text'
+            placeholder='Search Image Term...'
+            className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none'
+          />
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default ImageSearch
