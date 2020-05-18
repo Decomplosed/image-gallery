a621ecbb0661fb90ebb766b8a50bf2b72083d5c1
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-18 20:38:59 +0200

    created a container for search form

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ImageSearch = () => {
-  return <div></div>
+  return <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'></div>
 }
 
 export default ImageSearch
