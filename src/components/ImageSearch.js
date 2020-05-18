951542cd3386cc0f3a488d803e6543a646b6d5a7
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-18 20:39:26 +0200

    created form tag

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const ImageSearch = () => {
-  return <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'></div>
+  return <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+    <form className="w-full max-w-sm">
+      
+    </form>
+  </div>
 }
 
 export default ImageSearch
