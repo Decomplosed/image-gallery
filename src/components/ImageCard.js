127b66d76a80c83be1536916eabcb78fc61bca18
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-18 20:19:29 +0200

    trim the tags

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -31,7 +31,7 @@ const ImageCard = ({ image }) => {
             key={index}
             className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'
           >
-            #{tag}
+            #{tag.trim()}
           </span>
         ))}
       </div>
