f883c2a3d4530b449ead8767ca94c19fed4b86b3
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-18 20:17:00 +0200

    created tags array

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const ImageCard = ({ image }) => {
+  const tags = image.tags.split(',')
+
   return (
     <div className='max-w-sm rounded overflow-hidden shadow-lg'>
       <img src={image.webformatURL} alt='unsplash' className='w-full' />
