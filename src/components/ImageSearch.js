dc30e980b2d5dc9376e7624269937d6d728fe27b
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-18 20:42:59 +0200

    created button

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -10,6 +10,12 @@ const ImageSearch = () => {
             placeholder='Search Image Term...'
             className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none'
           />
+          <button
+            type='submit'
+            className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded'
+          >
+            Search
+          </button>
         </div>
       </form>
     </div>
